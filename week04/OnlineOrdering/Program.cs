5364aab4b035d0e4aada35586a2ee1a2c3011e1f
kevinvillajim <kevinvillajim@hotmail.com>
2025-01-31 15:43:49 -0500

    Activity week 4 ended

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -2,12 +2,12 @@ class Program
 {
     static void Main()
     {
-        Address address1 = new Address("123 Main St", "New York", "NY", "USA");
-        Customer customer1 = new Customer("John Doe", address1);
+        Address address1 = new Address("123 Av.", "Miami", "FL", "USA");
+        Customer customer1 = new Customer("John Wick", address1);
 
         Order order1 = new Order(customer1);
-        order1.AddProduct(new Product("Laptop", "L123", 1000, 1));
-        order1.AddProduct(new Product("Mouse", "M456", 50, 2));
+        order1.AddProduct(new Product("Business Suit", "L123", 1000, 3));
+        order1.AddProduct(new Product("Bullets", "M456", 30, 4));
 
         Console.WriteLine(order1.GeneratePackingLabel());
         Console.WriteLine(order1.GenerateShippingLabel());
